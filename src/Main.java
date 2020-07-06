2bf79c815ce0f02d2132258c9aae9e72238ea583
jimmy <jimmyjaytscheina@gmail.com>
2020-07-06 15:16:23 +0200

    Person and BankAccount

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
 public class Main {
+
+
+
+
+
     public static void main(String[] args) {
-        Person person1 = new Person("John", "Doe");
-        System.out.println (person1.toString());
+
+
+        Person firstName = new Person ("Johnny","Cash\n");
+        System.out.println(firstName);
+
+        getFullName fNames = new getFullName();
+
+
+        fNames.getName("John","Doe");
+        fNames.getName("Christian", "Sander");
+        fNames.getName("Sebastian", "Miller");
+
+        System.out.println("\n");
+
+
+        System.out.println("BANK ACCOUNT");
+        BankAccount2 chrisSavings = new BankAccount2(200);
+
+        chrisSavings.payment(45);
+        chrisSavings.payment(-300);
+        if (chrisSavings.getBalance()>= 0){
+            System.out.println("Your Balance is: "+chrisSavings.getBalance());
+        }else
+            System.out.println("WARNING overdraft, your debts are: "+(chrisSavings.getBalance()*-1));
+
+
+
+
+
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -2,14 +2,20 @@ public class Person {
     String firstName;
     String secondName;
 
+
+
     public Person (String firstName, String secondName){
 
         this.firstName = firstName;
         this.secondName = secondName;
     }
-
     @Override
-    public String toString() {
-        return super.toString();
+
+    public String toString(){
+    return firstName + " " + secondName;
+
     }
+
+
+
 }
